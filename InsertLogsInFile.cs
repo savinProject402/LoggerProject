6c9a8e9a85c520e0b6b5a4fea985a666f658057f
Олег Савін <oleh.savin@nure.ua>
2020-11-26 18:16:02 +0200

    insert code for Logs and MINcode for Class Logger

--- InsertLogsInFile.cs
+++ InsertLogsInFile.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace LoggerProject
 {
     class InsertLogsInFile
     {
+        public void WriteLogsIn(Logger logger) // нужен класс Logger
+            {
+            File.WriteAllText(DateTime.Today + ".txt", Logger.Task.ToString());
+            }
     }
 }
---
+++ Logger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerProject
+{
+    class Logger
+    {
+        public static Logger logger = new Logger();
+
+        private Logger()
+        {
+            MessageRead = new StringBuilder();
+        }
+        public StringBuilder MessageRead
+        {
+            get;  set;
+        }
+
+        public static Logger Task => logger;
+
+
+
+    }
+}
